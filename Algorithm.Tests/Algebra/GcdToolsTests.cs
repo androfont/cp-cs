52a25b9b2dd2379a2a37db18897c203b1425d221
Andro Font <androfont@gmail.com>
2021-09-25 19:23:42 -0300

    feat: Add gcd and lcm methods

---
+++ Algorithm.Tests/Algebra/GcdToolsTests.cs
@@ -0,0 +1,36 @@
+﻿using Algorithm.Algebra;
+using Xunit;
+
+namespace Algorithm.Tests.Algebra
+{
+    public class GcdToolsTests
+    {
+        [Fact]
+        public void Gcd_Basic_Ok()
+        {
+            // Arrange
+            int a = 100;
+            int b = 8;
+
+            // Act
+            int result = GcdTools.Gcd(a, b);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void Lcm_Basic_Ok()
+        {
+            // Arrange
+            int a = 100;
+            int b = 8;
+
+            // Act
+            int result = GcdTools.Lcm(a, b);
+
+            // Assert
+            Assert.Equal(200, result);
+        }
+    }
+}
---
+++ Algorithm/Algebra/GcdTools.cs
@@ -0,0 +1,23 @@
+﻿namespace Algorithm.Algebra
+{
+    public static class GcdTools
+    {
+        public static int Gcd(int a, int b)
+        {
+            while (b > 0)
+            {
+                a %= b;
+                // Swap a, b
+                int temp = b;
+                b = a;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static int Lcm(int a, int b)
+        {
+            return a / Gcd(a, b) * b;
+        }
+    }
+}
