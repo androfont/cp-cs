81ac6669423ed738de29f2b4771503ab78a39fec
Andro Font <androfont@gmail.com>
2021-09-25 19:34:48 -0300

    feat: Add extended euclidean method

--- Algorithm.Tests/Algebra/GcdToolsTests.cs
+++ Algorithm.Tests/Algebra/GcdToolsTests.cs
@@ -32,5 +32,21 @@ namespace Algorithm.Tests.Algebra
             // Assert
             Assert.Equal(200, result);
         }
+
+        [Fact]
+        public void GcdExtended_Basic_Ok()
+        {
+            // Arrange
+            int a = 240;
+            int b = 46;
+
+            // Act
+            int result = GcdTools.GcdExtended(a, b, out int x, out int y);
+
+            // Assert
+            Assert.Equal(2, result);
+            Assert.Equal(-9, x);
+            Assert.Equal(47, y);
+        }
     }
 }
