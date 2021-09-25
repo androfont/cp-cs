6b8aecf6ea32acda57afc6834e940bbce6cfa2f0
Andro Font <androfont@gmail.com>
2021-09-25 18:58:35 -0300

    feat: Add integer pow module

--- Algorithm.Tests/Algebra/BinaryExponentiationTests.cs
+++ Algorithm.Tests/Algebra/BinaryExponentiationTests.cs
@@ -55,5 +55,59 @@ namespace Algorithm.Tests.Algebra
             Assert.Equal(2048, result);
             Assert.Equal(-2048, negResult);
         }
+
+        [Fact]
+        public void IntPowMod_Pow2Exp_Ok()
+        {
+            // Arrange
+            long n = 2;
+            long neg = -2;
+            ulong e = 8;
+            long mod = 18;
+
+            // Act
+            long result = BinaryExponentiation.IntPowMod(n, e, mod);
+            long negResult = BinaryExponentiation.IntPowMod(neg, e, mod);
+
+            // Assert
+            Assert.Equal(4, result);
+            Assert.Equal(4, negResult);
+        }
+
+        [Fact]
+        public void IntPowMod_EvenExp_Ok()
+        {
+            // Arrange
+            long n = 2;
+            long neg = -2;
+            ulong e = 10;
+            long mod = 18;
+
+            // Act
+            long result = BinaryExponentiation.IntPowMod(n, e, mod);
+            long negResult = BinaryExponentiation.IntPowMod(neg, e, mod);
+
+            // Assert
+            Assert.Equal(16, result);
+            Assert.Equal(16, negResult);
+        }
+
+        [Fact]
+        public void IntPowMod_OddExp_Ok()
+        {
+            // Arrange
+            long n = 2;
+            long neg = -2;
+            ulong e = 11;
+            long mod = 18;
+
+            // Act
+            long result = BinaryExponentiation.IntPowMod(n, e, mod);
+            long negResult = BinaryExponentiation.IntPowMod(neg, e, mod);
+
+            // Assert
+            Assert.Equal(14, result);
+            Assert.Equal(-14, negResult);
+        }
     }
 }
--- Algorithm/Algebra/BinaryExponentiation.cs
+++ Algorithm/Algebra/BinaryExponentiation.cs
@@ -16,5 +16,21 @@
             }
             return result;
         }
+
+        public static long IntPowMod(long n, ulong e, long mod)
+        {
+            n %= mod;
+            long result = 1;
+            while (e > 0)
+            {
+                if ((e & 1) == 1)
+                {
+                    result = result * n % mod;
+                }
+                n = (n * n) % mod;
+                e >>= 1;
+            }
+            return result;
+        }
     }
 }
