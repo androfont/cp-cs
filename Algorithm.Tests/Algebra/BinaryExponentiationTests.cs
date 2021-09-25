d091569edc0d444d612a2df22c0daf7433c8712f
Andro Font <androfont@gmail.com>
2021-09-25 18:12:28 -0300

    Add project files.

---
+++ Algorithm.Tests/Algebra/BinaryExponentiationTests.cs
@@ -0,0 +1,59 @@
+using Algorithm.Algebra;
+using Xunit;
+
+namespace Algorithm.Tests.Algebra
+{
+    public class BinaryExponentiationTests
+    {
+        [Fact]
+        public void IntPow_Pow2Exp_Ok()
+        {
+            // Arrange
+            long n = 2;
+            long neg = -2;
+            ulong e = 8;
+
+            // Act
+            long result = BinaryExponentiation.IntPow(n, e);
+            long negResult = BinaryExponentiation.IntPow(neg, e);
+
+            // Assert
+            Assert.Equal(256, result);
+            Assert.Equal(256, negResult);
+        }
+
+        [Fact]
+        public void IntPow_EvenExp_Ok()
+        {
+            // Arrange
+            long n = 2;
+            long neg = -2;
+            ulong e = 10;
+
+            // Act
+            long result = BinaryExponentiation.IntPow(n, e);
+            long negResult = BinaryExponentiation.IntPow(neg, e);
+
+            // Assert
+            Assert.Equal(1024, result);
+            Assert.Equal(1024, negResult);
+        }
+
+        [Fact]
+        public void IntPow_OddExp_Ok()
+        {
+            // Arrange
+            long n = 2;
+            long neg = -2;
+            ulong e = 11;
+
+            // Act
+            long result = BinaryExponentiation.IntPow(n, e);
+            long negResult = BinaryExponentiation.IntPow(neg, e);
+
+            // Assert
+            Assert.Equal(2048, result);
+            Assert.Equal(-2048, negResult);
+        }
+    }
+}
---
+++ Algorithm/Algebra/BinaryExponentiation.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithm.Algebra
+{
+    public static class BinaryExponentiation
+    {
+        public static long IntPow(long n, ulong e)
+        {
+            long result = 1;
+            while (e > 0)
+            {
+                if ((e & 1) == 1)
+                {
+                    result *= n;
+                }
+                n *= n;
+                e >>= 1;
+            }
+            return result;
+        }
+    }
+}
